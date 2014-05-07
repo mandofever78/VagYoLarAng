22a7b1009575675c419870b99a3a952ba3657c7f
Nolan Lawrence <nolanlawrence@icloud.com>
2014-05-07 06:08:57 -0500

    Working, needs grunt installed locally

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,3 +1,6 @@
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$ruby_version = '2.1.1'
+
 class must-have {
   include apt
 
@@ -13,17 +16,6 @@ class must-have {
     require => Apt::Ppa["ppa:chris-lea/node.js"],
   }
 
-  exec { 'install gem compass':
-    command => '/opt/ruby/bin/gem install compass',
-    creates => "/opt/ruby/bin/compass",
-    require => Exec["apt-get update 2"],
-  }
-
-  exec { 'install gem sass':
-    command => '/opt/ruby/bin/gem install sass',
-    creates => "/opt/ruby/bin/sass",
-    require => Exec["apt-get update 2"],
-  }
 
   exec { 'install yeoman':
     command => '/usr/bin/npm install -g yo phantomjs',
@@ -36,42 +28,68 @@ class must-have {
     require => [ Exec["apt-get update 2"], Package["nodejs"] ],
   }
 
-  exec { 'install webapp generator':
-    command => '/usr/bin/npm install -g generator-webapp',
-    creates => '/usr/lib/node_modules/generator-webapp',
+  exec { 'install angular generator':
+    command => '/usr/bin/npm install -g generator-angular',
+    creates => '/usr/lib/node_modules/generator-angular',
     require => Exec["install yeoman"],
   }
 
-  file { "/home/vagrant/yeoman/webapp":
+  file { "/home/vagrant/angular":
       ensure => "directory",
-      before => Exec['create webapp site'],
-      require => Exec['install webapp generator'],
+      before => Exec['create angular site'],
+      require => Exec['install angular generator'],
   }
 
-  exec { 'create webapp site':
-    command => '/usr/bin/yes | /usr/lib/node_modules/yo/bin/yo webapp',
-    cwd => '/home/vagrant/yeoman/webapp',
-    creates => '/home/vagrant/yeoman/webapp/app',
-    require => File["/home/vagrant/yeoman/webapp"],
+  exec { 'create angular site':
+    command => '/usr/bin/yes | /usr/bin/yo angular',
+    cwd => '/home/vagrant/angular',
+    creates => '/home/vagrant/angular/app',
+    require => File["/home/vagrant/angular"],
+    logoutput => true,
+    returns => [0, 8]
   }
 
   file_line { "update hostname in gruntfile": 
     line => "\t\t\t\thostname: '0.0.0.0'", 
-    path => "/home/vagrant/yeoman/webapp/Gruntfile.js", 
+    path => "/home/vagrant/angular/Gruntfile.js", 
     match => "hostname: '.*'", 
     ensure => present,
-    require => Exec["create webapp site"],
+    require => Exec["create angular site"],
   }
 
-  package { ["vim",
+  package { ["wget",
+             "curl",
              "bash",
              "nodejs",
              "git-core",
+             "build-essential",
              "fontconfig"]:
     ensure => present,
     require => Exec["apt-get update 2"],
   }
 
+ # --- Ruby ---------------------------------------------------------------------
+
+Exec {
+  path => '/usr/local/bin:/usr/bin:/bin',
+}
+
+#    exec { 'install_rvm':
+#      command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+#      creates => "/.rvm/bin/rvm",
+#      require => Package['curl']
+#    }
+
+#    exec { 'install_ruby':
+      # We run the rvm executable directly because the shell function assumes an
+      # interactive environment, in particular to display messages or ask questions.
+      # The rvm executable is more suitable for automated installs.
+      #
+      # use a ruby patch level known to have a binary
+#      command => "${as_vagrant} '/home/vagrant/.rvm/bin/rvm install ruby-${ruby_version} --#binary --autolibs=enabled && rvm alias create default ${ruby_version}'",
+#      creates => "/.rvm/bin/ruby",
+#      require => Exec['install_rvm']
+#    }
 
 }
 
